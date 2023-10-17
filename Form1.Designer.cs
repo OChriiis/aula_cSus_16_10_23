c91cde63e91ee1ca38d53c72afe08eebbc1bfdd7
Herick Anderson Dos Santos <77398244+OChriiis@users.noreply.github.com>
2023-10-16 21:25:02 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace CadastroSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonDeletar = new System.Windows.Forms.Button();
+            this.buttonAlterar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSalvar.Location = new System.Drawing.Point(177, 76);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(166, 62);
+            this.buttonSalvar.TabIndex = 0;
+            this.buttonSalvar.Text = "SALVAR";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonDeletar
+            // 
+            this.buttonDeletar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeletar.Location = new System.Drawing.Point(177, 248);
+            this.buttonDeletar.Name = "buttonDeletar";
+            this.buttonDeletar.Size = new System.Drawing.Size(166, 62);
+            this.buttonDeletar.TabIndex = 1;
+            this.buttonDeletar.Text = "DELETAR";
+            this.buttonDeletar.UseVisualStyleBackColor = true;
+            this.buttonDeletar.Click += new System.EventHandler(this.buttonDeletar_Click);
+            // 
+            // buttonAlterar
+            // 
+            this.buttonAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAlterar.Location = new System.Drawing.Point(177, 163);
+            this.buttonAlterar.Name = "buttonAlterar";
+            this.buttonAlterar.Size = new System.Drawing.Size(166, 62);
+            this.buttonAlterar.TabIndex = 2;
+            this.buttonAlterar.Text = "ALTERAR";
+            this.buttonAlterar.Click += new System.EventHandler(this.buttonAlterar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAlterar);
+            this.Controls.Add(this.buttonDeletar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonDeletar;
+        private System.Windows.Forms.Button buttonAlterar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace CadastroSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            //insert 
+            string sql = "insert into aluno (nome) values ('Herick')";
+
+            MySqlConnection con = new MySqlConnection("server=localhost;uid=root;pwd='';database='uninove'");
+
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery(); // insert / update / delete
+            // cmd.ExecuteReader(); // para select
+
+
+            MessageBox.Show("Dados inseridos com sucesso!");
+
+            con.Close();
+            con.Dispose();
+            cmd.Dispose();
+
+        }
+
+        //delete
+
+        private void buttonDeletar_Click(object sender, EventArgs e)
+            {
+
+            //delete
+            string sql = "delete from aluno where 1";
+
+            MySqlConnection con = new MySqlConnection("server=localhost;uid=root;pwd='';database='uninove'");
+
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery(); // insert / update / delete
+            // cmd.ExecuteReader(); // para select
+
+            MessageBox.Show("Dados excluidos com sucesso!");
+
+            con.Close();
+            con.Dispose();
+            cmd.Dispose();
+
+        }
+
+        // alterar
+        private void buttonAlterar_Click(object sender, EventArgs e)
+        {
+
+            //delete
+            string sql = "update aluno set nome = 'Marcus'";
+
+            MySqlConnection con = new MySqlConnection("server=localhost;uid=root;pwd='';database='uninove'");
+
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery(); // insert / update / delete
+            // cmd.ExecuteReader(); // para select
+
+            MessageBox.Show("Dados alterados com sucesso!");
+
+            con.Close();
+            con.Dispose();
+            cmd.Dispose();
+
+        }
+
+
+    }
+}
